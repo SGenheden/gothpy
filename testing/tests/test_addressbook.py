2b1ff59c78666a1e9f5c839f4e63e1cbdc556f9c
Samuel Genheden <samuel.genheden@gmail.com>
2019-03-30 16:18:10 +0100

    pytest part 3

--- testing/tests/test_addressbook.py
+++ testing/tests/test_addressbook.py
@@ -53,3 +53,53 @@ def test_save_load(simple_addressbook, tmpdir):
     a2 = AddressBook()
     a2.load(filename)
     assert a2.to_dict() == a1.to_dict()
+
+
+def test_save(simple_addressbook, mocker):
+    patched_dump = mocker.patch("addressbook.addressbook.json.dumps")
+    fileobj = mocker.MagicMock()
+    patched_open = mocker.patch("builtins.open", return_value=fileobj)
+    filename = "temp.json"
+    simple_addressbook.save(filename)
+    patched_open.called_once_with(filename, "w")
+    patched_dump.called_once_with(simple_addressbook.to_dict(), fileobj)
+    assert not os.path.exists(filename)
+
+
+def test_open(simple_addressbook, mocker):
+    patched_load = mocker.patch(
+        "addressbook.addressbook.json.load", return_value=simple_addressbook.to_dict()
+    )
+    fileobj = mocker.MagicMock()
+    patched_open = mocker.patch("builtins.open", return_value=fileobj)
+    a2 = AddressBook()
+    a2.load("temp.json")
+    patched_open.called_once_with("temp.json", "r")
+    patched_load.called_once_with(fileobj)
+    assert a2.to_dict() == simple_addressbook.to_dict()
+
+
+def test_sync(simple_addressbook, mocker):
+    patched_api = mocker.patch(
+        "addressbook.addressbook.CloudAddressBookApi", autospec=True
+    )
+    patched_api.return_value.download.return_value = {
+        "donald": {"address": "pennsylvania avenue"}
+    }
+
+    simple_addressbook.sync("usr:pwd")
+    patched_api.assert_called_once()
+    patched_api.return_value.download.assert_called_once()
+    patched_api.return_value.upload.assert_called_once_with(simple_addressbook)
+    assert simple_addressbook.query("donald")
+
+
+def test_sync_failure(simple_addressbook, mocker):
+    patched_api = mocker.patch(
+        "addressbook.addressbook.CloudAddressBookApi", autospec=True
+    )
+    patched_api.return_value.download = mocker.MagicMock(
+        side_effect=ConnectionError("Unable to connect to API")
+    )
+    with pytest.raises(ConnectionError):
+        simple_addressbook.sync("usr:pwd")
